f718f9548d9b5e41b69cd9b23932e9822c08f7c3
Alina <pisarenko.alina@gmail.com>
2022-09-13 20:20:24 -0400

    implemented review section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,59 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div className='footer'>Footer</div>;
+  return (
+    <div className='footer'>
+      <ul className='nav'>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Find your dream home
+          </a>
+        </li>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Request proposal
+          </a>
+        </li>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Download home planner
+          </a>
+        </li>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Contact us
+          </a>
+        </li>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Submit your property
+          </a>
+        </li>
+        <li className='nav__item'>
+          <a href='#' className='nav__link'>
+            Come work with us!
+          </a>
+        </li>
+      </ul>
+      <p className='copyright'>
+        This project created by{' '}
+        <a
+          className=' footer__link'
+          href='https://www.alinapisarenko.com/'
+          target='_blank'
+        >
+          Alina Pisarenko{' '}
+        </a>{' '}
+        and is open-sourced on{' '}
+        <a
+          className='footer__link'
+          href='https://github.com/AlinaPisarenko/explore-app'
+          target='_blank'
+        >
+          {' '}
+          GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import imgOne from '../images/cpl-img.jpeg';
+import imgTwo from '../images/house-img.webp';
 
 export default function Review() {
-  return <div className='review'>Review</div>;
+  return (
+    <>
+      <div className='review__pictures'>
+        <img
+          src={imgTwo}
+          alt='Couple with new house'
+          className='review__img--1'
+        />
+
+        <img src={imgOne} alt='New house' className='review__img--2' />
+      </div>
+
+      <div className='review__content'>
+        <h3 className='heading-3 mb-sm'>Happy Customers</h3>
+        <h2 className='heading-2 heading-2--dark mb-md'>
+          &ldquo;The best decision of our lives&rdquo;
+        </h2>
+        <p className='review__text'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+          harum volupta!
+        </p>
+        <button className='btn'>Find your own home</button>
+      </div>
+    </>
+  );
 }
